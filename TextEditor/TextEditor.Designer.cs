d61ad7978619a7826fa63dc78386a7bd447a8404
Daniel Jason Purcell <purcelldj@gmail.com>
2013-04-05 17:19:06 -0500

    first commit

---
+++ TextEditor/TextEditor.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace TextEditor
+{
+    partial class frmTextEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.mnuMain = new System.Windows.Forms.MenuStrip();
+            this.mnuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuSaveAs = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuEdit = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuCut = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuCopy = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuPaste = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuDelete = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuSelectAll = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuHelp = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.cmMain = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.cmCut = new System.Windows.Forms.ToolStripMenuItem();
+            this.cmCopy = new System.Windows.Forms.ToolStripMenuItem();
+            this.cmPaste = new System.Windows.Forms.ToolStripMenuItem();
+            this.cmDelete = new System.Windows.Forms.ToolStripMenuItem();
+            this.cmSelectAll = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtBox = new System.Windows.Forms.RichTextBox();
+            this.dlgSave = new System.Windows.Forms.SaveFileDialog();
+            this.dlgOpen = new System.Windows.Forms.OpenFileDialog();
+            this.mnuMain.SuspendLayout();
+            this.cmMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mnuMain
+            // 
+            this.mnuMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFile,
+            this.mnuEdit,
+            this.mnuHelp});
+            this.mnuMain.Location = new System.Drawing.Point(0, 0);
+            this.mnuMain.Name = "mnuMain";
+            this.mnuMain.Size = new System.Drawing.Size(635, 24);
+            this.mnuMain.TabIndex = 0;
+            // 
+            // mnuFile
+            // 
+            this.mnuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuNew,
+            this.mnuOpen,
+            this.mnuSave,
+            this.mnuSaveAs,
+            this.mnuExit});
+            this.mnuFile.Name = "mnuFile";
+            this.mnuFile.Size = new System.Drawing.Size(37, 20);
+            this.mnuFile.Text = "&File";
+            // 
+            // mnuNew
+            // 
+            this.mnuNew.Name = "mnuNew";
+            this.mnuNew.ShortcutKeyDisplayString = "Ctrl+N";
+            this.mnuNew.Size = new System.Drawing.Size(146, 22);
+            this.mnuNew.Text = "&New";
+            this.mnuNew.Click += new System.EventHandler(this.mnuNew_Click);
+            // 
+            // mnuOpen
+            // 
+            this.mnuOpen.Name = "mnuOpen";
+            this.mnuOpen.ShortcutKeyDisplayString = "Ctrl+O";
+            this.mnuOpen.Size = new System.Drawing.Size(146, 22);
+            this.mnuOpen.Text = "&Open";
+            this.mnuOpen.Click += new System.EventHandler(this.mnuOpen_Click);
+            // 
+            // mnuSave
+            // 
+            this.mnuSave.Name = "mnuSave";
+            this.mnuSave.ShortcutKeyDisplayString = "Ctrl+S";
+            this.mnuSave.Size = new System.Drawing.Size(146, 22);
+            this.mnuSave.Text = "&Save";
+            this.mnuSave.Click += new System.EventHandler(this.mnuSave_Click);
+            // 
+            // mnuSaveAs
+            // 
+            this.mnuSaveAs.Name = "mnuSaveAs";
+            this.mnuSaveAs.Size = new System.Drawing.Size(146, 22);
+            this.mnuSaveAs.Text = "Save &As";
+            this.mnuSaveAs.Click += new System.EventHandler(this.mnuSaveAs_Click);
+            // 
+            // mnuExit
+            // 
+            this.mnuExit.Name = "mnuExit";
+            this.mnuExit.ShortcutKeyDisplayString = "Ctrl+Q";
+            this.mnuExit.Size = new System.Drawing.Size(146, 22);
+            this.mnuExit.Text = "E&xit";
+            this.mnuExit.Click += new System.EventHandler(this.mnuExit_Click);
+            // 
+            // mnuEdit
+            // 
+            this.mnuEdit.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuCut,
+            this.mnuCopy,
+            this.mnuPaste,
+            this.mnuDelete,
+            this.mnuSelectAll});
+            this.mnuEdit.Name = "mnuEdit";
+            this.mnuEdit.Size = new System.Drawing.Size(39, 20);
+            this.mnuEdit.Text = "&Edit";
+            // 
+            // mnuCut
+            // 
+            this.mnuCut.Name = "mnuCut";
+            this.mnuCut.ShortcutKeyDisplayString = "Ctrl+X";
+            this.mnuCut.Size = new System.Drawing.Size(164, 22);
+            this.mnuCut.Text = "Cu&t";
+            this.mnuCut.Click += new System.EventHandler(this.mnuCut_Click);
+            // 
+            // mnuCopy
+            // 
+            this.mnuCopy.Name = "mnuCopy";
+            this.mnuCopy.ShortcutKeyDisplayString = "Ctrl+C";
+            this.mnuCopy.Size = new System.Drawing.Size(164, 22);
+            this.mnuCopy.Text = "&Copy";
+            this.mnuCopy.Click += new System.EventHandler(this.mnuCopy_Click);
+            // 
+            // mnuPaste
+            // 
+            this.mnuPaste.Name = "mnuPaste";
+            this.mnuPaste.ShortcutKeyDisplayString = "Ctrl+V";
+            this.mnuPaste.Size = new System.Drawing.Size(164, 22);
+            this.mnuPaste.Text = "&Paste";
+            this.mnuPaste.Click += new System.EventHandler(this.mnuPaste_Click);
+            // 
+            // mnuDelete
+            // 
+            this.mnuDelete.Name = "mnuDelete";
+            this.mnuDelete.ShortcutKeyDisplayString = "Del";
+            this.mnuDelete.Size = new System.Drawing.Size(164, 22);
+            this.mnuDelete.Text = "&Delete";
+            this.mnuDelete.Click += new System.EventHandler(this.mnuDelete_Click);
+            // 
+            // mnuSelectAll
+            // 
+            this.mnuSelectAll.Name = "mnuSelectAll";
+            this.mnuSelectAll.ShortcutKeyDisplayString = "Ctrl+A";
+            this.mnuSelectAll.Size = new System.Drawing.Size(164, 22);
+            this.mnuSelectAll.Text = "Select &All";
+            this.mnuSelectAll.Click += new System.EventHandler(this.mnuSelectAll_Click);
+            // 
+            // mnuHelp
+            // 
+            this.mnuHelp.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuAbout});
+            this.mnuHelp.Name = "mnuHelp";
+            this.mnuHelp.Size = new System.Drawing.Size(44, 20);
+            this.mnuHelp.Text = "&Help";
+            // 
+            // mnuAbout
+            // 
+            this.mnuAbout.Name = "mnuAbout";
+            this.mnuAbout.Size = new System.Drawing.Size(107, 22);
+            this.mnuAbout.Text = "&About";
+            this.mnuAbout.Click += new System.EventHandler(this.mnuAbout_Click);
+            // 
+            // cmMain
+            // 
+            this.cmMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cmCut,
+            this.cmCopy,
+            this.cmPaste,
+            this.cmDelete,
+            this.cmSelectAll});
+            this.cmMain.Name = "contextMenuStrip1";
+            this.cmMain.Size = new System.Drawing.Size(165, 114);
+            // 
+            // cmCut
+            // 
+            this.cmCut.Name = "cmCut";
+            this.cmCut.ShortcutKeyDisplayString = "Ctrl+X";
+            this.cmCut.Size = new System.Drawing.Size(164, 22);
+            this.cmCut.Text = "Cut";
+            this.cmCut.Click += new System.EventHandler(this.cmCut_Click);
+            // 
+            // cmCopy
+            // 
+            this.cmCopy.Name = "cmCopy";
+            this.cmCopy.ShortcutKeyDisplayString = "Ctrl+C";
+            this.cmCopy.Size = new System.Drawing.Size(164, 22);
+            this.cmCopy.Text = "Copy";
+            this.cmCopy.Click += new System.EventHandler(this.cmCopy_Click);
+            // 
+            // cmPaste
+            // 
+            this.cmPaste.Name = "cmPaste";
+            this.cmPaste.ShortcutKeyDisplayString = "Ctrl+V";
+            this.cmPaste.Size = new System.Drawing.Size(164, 22);
+            this.cmPaste.Text = "Paste";
+            this.cmPaste.Click += new System.EventHandler(this.cmPaste_Click);
+            // 
+            // cmDelete
+            // 
+            this.cmDelete.Name = "cmDelete";
+            this.cmDelete.ShortcutKeyDisplayString = "Del";
+            this.cmDelete.Size = new System.Drawing.Size(164, 22);
+            this.cmDelete.Text = "Delete";
+            this.cmDelete.Click += new System.EventHandler(this.cmDelete_Click);
+            // 
+            // cmSelectAll
+            // 
+            this.cmSelectAll.Name = "cmSelectAll";
+            this.cmSelectAll.ShortcutKeyDisplayString = "Ctrl+A";
+            this.cmSelectAll.Size = new System.Drawing.Size(164, 22);
+            this.cmSelectAll.Text = "Select All";
+            this.cmSelectAll.Click += new System.EventHandler(this.cmSelectAll_Click);
+            // 
+            // txtBox
+            // 
+            this.txtBox.AcceptsTab = true;
+            this.txtBox.ContextMenuStrip = this.cmMain;
+            this.txtBox.DetectUrls = false;
+            this.txtBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtBox.Location = new System.Drawing.Point(0, 24);
+            this.txtBox.Name = "txtBox";
+            this.txtBox.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txtBox.Size = new System.Drawing.Size(635, 435);
+            this.txtBox.TabIndex = 1;
+            this.txtBox.Text = "";
+            this.txtBox.TextChanged += new System.EventHandler(this.txtBox_TextChanged);
+            this.txtBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtBox_KeyDown);
+            // 
+            // dlgSave
+            // 
+            this.dlgSave.DefaultExt = "txt";
+            this.dlgSave.Filter = "Text files|*.txt|All files|*.*";
+            this.dlgSave.SupportMultiDottedExtensions = true;
+            // 
+            // dlgOpen
+            // 
+            this.dlgOpen.Filter = "Text files|*.txt|All files|*.*";
+            // 
+            // frmTextEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(635, 459);
+            this.Controls.Add(this.txtBox);
+            this.Controls.Add(this.mnuMain);
+            this.MainMenuStrip = this.mnuMain;
+            this.Name = "frmTextEditor";
+            this.mnuMain.ResumeLayout(false);
+            this.mnuMain.PerformLayout();
+            this.cmMain.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mnuMain;
+        private System.Windows.Forms.ToolStripMenuItem mnuFile;
+        private System.Windows.Forms.ToolStripMenuItem mnuNew;
+        private System.Windows.Forms.ToolStripMenuItem mnuOpen;
+        private System.Windows.Forms.ToolStripMenuItem mnuSave;
+        private System.Windows.Forms.ToolStripMenuItem mnuSaveAs;
+        private System.Windows.Forms.ToolStripMenuItem mnuExit;
+        private System.Windows.Forms.ContextMenuStrip cmMain;
+        private System.Windows.Forms.ToolStripMenuItem cmCut;
+        private System.Windows.Forms.ToolStripMenuItem cmCopy;
+        private System.Windows.Forms.ToolStripMenuItem cmPaste;
+        private System.Windows.Forms.ToolStripMenuItem cmDelete;
+        private System.Windows.Forms.ToolStripMenuItem mnuEdit;
+        private System.Windows.Forms.ToolStripMenuItem mnuCut;
+        private System.Windows.Forms.ToolStripMenuItem mnuCopy;
+        private System.Windows.Forms.ToolStripMenuItem mnuPaste;
+        private System.Windows.Forms.ToolStripMenuItem mnuDelete;
+        private System.Windows.Forms.ToolStripMenuItem mnuHelp;
+        private System.Windows.Forms.ToolStripMenuItem mnuAbout;
+        private System.Windows.Forms.RichTextBox txtBox;
+        private System.Windows.Forms.ToolStripMenuItem mnuSelectAll;
+        private System.Windows.Forms.ToolStripMenuItem cmSelectAll;
+        private System.Windows.Forms.SaveFileDialog dlgSave;
+        private System.Windows.Forms.OpenFileDialog dlgOpen;
+    }
+}
+
---
+++ TextEditor/TextEditor.cs
@@ -0,0 +1,274 @@
+﻿// Text Editor
+// Basic text editor with cut, copy, paste, and saving functions.
+// April 5, 2013
+// Daniel Jason Purcell
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextEditor
+{
+    public partial class frmTextEditor : Form
+    {
+        private String filename;
+        private bool changesMade;
+
+        public frmTextEditor()
+        {
+            InitializeComponent();
+            filename = null;
+            this.Text = "New Document";
+            changesMade = false;
+        }
+
+
+        private void cutText()
+        {
+            if (txtBox.SelectedText != "")
+            {
+                Clipboard.SetText(txtBox.SelectedText);
+                txtBox.SelectedText = "";
+            }
+        }
+
+        private void copyText()
+        {
+            if (txtBox.SelectedText != "")
+                Clipboard.SetText(txtBox.SelectedText);
+        }
+
+        private void pasteText()
+        {
+            if (Clipboard.GetText() != "")
+                txtBox.SelectedText = Clipboard.GetText();
+            
+        }
+
+        private void deleteText()
+        {
+            if (txtBox.SelectedText != "")
+                txtBox.SelectedText = "";
+            
+        }
+
+        private void selectAllText()
+        {
+            txtBox.SelectAll();
+        }
+
+        private void newDocument()
+        {
+            if (changesMade)
+            {
+                DialogResult result = MessageBox.Show("You have made changes to the document. \n" + "Would you like to save?",
+                    "Save?", MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.Yes)
+                    saveDocument();
+            }
+
+            filename = null;
+            this.Text = "New Document";
+            txtBox.Clear();
+            changesMade = false;
+
+        }
+
+        private void openDocument()
+        {
+
+            if (changesMade)
+            {
+                DialogResult result = MessageBox.Show("You have made changes to the document. \n" + "Would you like to save?", 
+                    "Save?", MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.Yes)
+                    saveDocument();
+            }
+
+            if (dlgOpen.ShowDialog() == DialogResult.OK)
+            {
+                filename = dlgOpen.FileName;
+                this.Text = System.IO.Path.GetFileName(filename);
+                System.IO.Stream stream = dlgOpen.OpenFile();
+                System.IO.StreamReader read = new System.IO.StreamReader(stream);
+                txtBox.Clear();
+                while (!read.EndOfStream)
+                    txtBox.Text += read.ReadLine() + "\n";
+                changesMade = false;
+
+            }
+
+        }
+
+        private void saveDocument()
+        {
+            if (filename == null)
+                saveAs();
+            else
+            {
+                System.IO.File.WriteAllLines(filename, txtBox.Lines);
+                changesMade = false;
+            }
+        }
+
+        private void saveAs()
+        {
+            if (dlgSave.ShowDialog() == DialogResult.OK)
+            {
+                filename = dlgSave.FileName;
+                this.Text = System.IO.Path.GetFileName(filename);
+                System.IO.File.WriteAllLines(filename, txtBox.Lines);
+                changesMade = false;
+            }
+            
+        }
+
+        private void exitDocument()
+        {
+            if (changesMade)
+            {
+                DialogResult result = MessageBox.Show("You have made changes to the document. \n" + "Would you like to save?",
+                    "Save?", MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.Yes)
+                    saveDocument();
+            }
+
+            this.Close();
+        }
+
+        private void aboutPrompt()
+        {
+            MessageBox.Show("Text Editor by Daniel Jason Purcell");
+        }
+
+        private void mnuCut_Click(object sender, EventArgs e)
+        {
+            cutText();
+        }
+
+        private void mnuCopy_Click(object sender, EventArgs e)
+        {
+            copyText();
+        }
+
+        private void mnuPaste_Click(object sender, EventArgs e)
+        {
+            pasteText();
+        }
+
+        private void mnuDelete_Click(object sender, EventArgs e)
+        {
+            deleteText();
+        }
+
+        private void mnuSelectAll_Click(object sender, EventArgs e)
+        {
+            selectAllText();
+        }
+
+        private void cmCut_Click(object sender, EventArgs e)
+        {
+            cutText();
+        }
+
+        private void cmCopy_Click(object sender, EventArgs e)
+        {
+            copyText();
+        }
+
+        private void cmPaste_Click(object sender, EventArgs e)
+        {
+            pasteText();
+        }
+
+        private void cmDelete_Click(object sender, EventArgs e)
+        {
+            deleteText();
+        }
+
+        private void cmSelectAll_Click(object sender, EventArgs e)
+        {
+            selectAllText();
+        }
+
+        private void txtBox_TextChanged(object sender, EventArgs e)
+        {
+            changesMade = true;
+        }
+
+        private void mnuNew_Click(object sender, EventArgs e)
+        {
+            newDocument();
+        }
+
+        private void mnuSaveAs_Click(object sender, EventArgs e)
+        {
+            saveAs();
+        }
+
+        private void mnuOpen_Click(object sender, EventArgs e)
+        {
+            openDocument();
+        }
+
+        private void mnuSave_Click(object sender, EventArgs e)
+        {
+            saveDocument();
+        }
+
+        private void mnuExit_Click(object sender, EventArgs e)
+        {
+            exitDocument();
+        }
+
+        private void mnuAbout_Click(object sender, EventArgs e)
+        {
+            aboutPrompt();
+        }
+
+        private void txtBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Modifiers == Keys.Control)
+                switch (e.KeyCode)
+                {
+                    case Keys.N:
+                        newDocument();
+                        break;
+                    case Keys.O:
+                        openDocument();
+                        break;
+                    case Keys.S:
+                        saveDocument();
+                        break;
+                    case Keys.Q:
+                        exitDocument();
+                        break;
+                    case Keys.X:
+                        cutText();
+                        break;
+                    case Keys.C:
+                        copyText();
+                        break;
+                    case Keys.V:
+                        pasteText();
+                        break;
+                    case Keys.A:
+                        selectAllText();
+                        break;
+                }
+        }
+
+        
+
+       
+    }
+}
